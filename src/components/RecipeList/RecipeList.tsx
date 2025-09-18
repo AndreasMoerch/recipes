d213667d33cddd9af09be0f18d65b1cad4c856fc
Andreas Mørch <andreas.morch92@gmail.com>
2025-09-18 17:23:41 +0200

    feat: Introduce Recipe Detail page, ROBOT.txt and other cool stuff

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Recipe } from '../../types/Recipe';
 import { allRecipes } from '../../data/allRecipes';
 
 /**
- * Component that displays a list of all available recipes, without caring about specific details (e.g. steps) about it.
+ * Component that displays a list of all available recipes
+ * Each recipe name is a clickable link to the recipe detail page
  */
 const RecipeList: React.FC = () => {
-
+  const recipes: Recipe[] = allRecipes;
 
   return (
     <div>
-      {allRecipes.map((recipe) => (
-        <div key={recipe.id}>{recipe.name}</div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+        </div>
       ))}
     </div>
   );
