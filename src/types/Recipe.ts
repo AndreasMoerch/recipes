6f86b97c3f1db12879b94143890a02dcf951b907
Andreas Mørch <andreas.morch92@gmail.com>
2025-09-18 21:07:11 +0200

    feat: make unit required

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -6,8 +6,8 @@ export interface Ingredient {
   name: string;
   /** The amount/quantity (e.g., "2", "1/2", "3") */
   amount: string;
-  /** The unit of measurement (e.g., "cups", "tbsp", "large", "cloves") */
-  unit?: string;
+  /** The unit of measurement (e.g., "cups", "tbsp", "large", "cloves", "whole", "pinch") */
+  unit: string;
 }
 
 /**
