bd3379a5e39e7c54f69667548cda0b7a7bd4594d
Andreas Mørch <andreas.morch92@gmail.com>
2025-09-18 21:01:07 +0200

    feat: add ingredient list to recipe model and render them

--- src/data/recipes/spaghettiCarbonara.ts
+++ src/data/recipes/spaghettiCarbonara.ts
@@ -2,5 +2,13 @@ import { Recipe } from '../../types/Recipe';
 
 export const spaghettiCarbonara: Recipe = {
   id: "spaghetti-carbonara",
-  name: "Spaghetti Carbonara"
+  name: "Spaghetti Carbonara",
+  ingredients: [
+    { name: "spaghetti", amount: "400", unit: "g" },
+    { name: "pancetta", amount: "150", unit: "g" },
+    { name: "eggs", amount: "3", unit: "large" },
+    { name: "parmesan cheese", amount: "100", unit: "g" },
+    { name: "black pepper", amount: "1", unit: "tsp" },
+    { name: "salt", amount: "1", unit: "tsp" }
+  ]
 };
